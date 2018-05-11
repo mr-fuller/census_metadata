fc02db26a53d4952337687d0dc6d7f33c7fa2449
fullerm <mrfuller460@gmail.com>
2018-05-11 12:27:10 -0400

    error handling if not acs5

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests, pandas
 # get a pandas dataframe of Census Metadata
-def getCensusMetadata(name,vintage, type="variables"):
+def getCensusMetadata(name,vintage=2016, type="variables"):
     def buildURL(name, vintage):
-        if vintage is '':
-            api_url = "https://api.census.gov/data/" + name
+        if name != 'acs5':
+            print("Error: API not yet supported.")
+            quit()
+        elif (vintage is None):
+            print("Invalid ACS year.")
+            quit()
         elif vintage in[2009,2010,2012,2013,2014]:
             api_url = "https://api.census.gov/data/" + str(vintage) +  '/' + name
         else:
             api_url = "https://api.census.gov/data/" + str(vintage) + "/" + name[:3] + '/' + name
         return api_url
+
         # Handle messy URLs?
 
     apiurl = buildURL(name,vintage)
@@ -17,12 +22,16 @@ def getCensusMetadata(name,vintage, type="variables"):
     print(u)
     df = pandas.DataFrame(requests.get(u).json()['variables']).transpose()
     return df
-
-print(getCensusMetadata('acs5',2016).shape)
-print(getCensusMetadata('acs5',2015).shape)
-print(getCensusMetadata('acs5',2014).shape)
-print(getCensusMetadata('acs5',2013).shape)
-print(getCensusMetadata('acs5',2012).shape)
-print(getCensusMetadata('acs5',2011).shape)
-print(getCensusMetadata('acs5',2010).shape)
-print(getCensusMetadata('acs5',2009).shape)
+# These are tests
+# print(getCensusMetadata('acs5',2016).shape)
+# print(getCensusMetadata('acs5',2015).shape)
+# print(getCensusMetadata('acs5',2014).shape)
+# print(getCensusMetadata('acs5',2013).shape)
+# print(getCensusMetadata('acs5',2012).shape)
+# print(getCensusMetadata('acs5',2011).shape)
+# print(getCensusMetadata('acs5',2010).shape)
+# print(getCensusMetadata('acs5',2009).shape)
+# print(getCensusMetadata('acs1',2009).shape)
+# print(getCensusMetadata('acs5').shape)
+# print(getCensusMetadata('acs5',vintage=None).shape)
+# print(getCensusMetadata('acs5',vintage='2015').shape)
