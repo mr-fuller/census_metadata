6365af0155151a7ae511a94c5b358b5d5ba8fee8
fullerm <mrfuller460@gmail.com>
2018-06-04 16:19:33 -0400

    add support for acs1 2012-present, sf3 1990-2000, and sf1 1990-2010;

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import requests, pandas
 # get a pandas dataframe of Census Metadata
 def getCensusMetadata(name,vintage=2016, type="variables"):
-    if name != 'acs5':
+    if name not in ['acs5','acs1','sf1','sf3']:
         print("Error: API not yet supported.")
         quit()
     elif (vintage is None):
         print("Invalid ACS year.")
         quit()
+    elif name == 'acs1' and vintage < 2012:
+        print("Invalid ACS year.")
+        quit()
+    elif name in ['sf1','sf3'] and vintage not in [2010,2000,1990]:
+        print("Invalid Census year.")
+        quit()
     else:
         def buildURL():
 
-            if vintage in[2009,2010,2012,2013,2014]:
+            if vintage in[1990,2000,2009,2010,2012,2013,2014]:
                 api_url = "https://api.census.gov/data/" + str(vintage) +  '/' + name
             else:
                 api_url = "https://api.census.gov/data/" + str(vintage) + "/" + name[:3] + '/' + name
@@ -27,6 +33,6 @@ def getCensusMetadata(name,vintage=2016, type="variables"):
 # These are tests
 
 # print(getCensusMetadata('acs1',2009).shape)
-print(getCensusMetadata('acs5',2012).columns.values)
+print(getCensusMetadata('acs1',2012).shape)
 # print(getCensusMetadata('acs5',vintage=None).shape)
 # print(getCensusMetadata('acs5',vintage='2015').shape)
